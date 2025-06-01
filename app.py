a19f3123f2d88aeab94d026778c61b6be0cc6d85
Eva <sherry921213@gmail.com>
2025-06-01 20:21:19 +0800

    try3

--- app.py
+++ app.py
@@ -136,11 +136,11 @@ def list_user_vm_api():
 @jwt_required()
 def create_user_vm_api():
     user_id = get_jwt_identity()   # 目前登入者的帳號
-    vmid = create_vm(user_id)["vmid"]  # 建立 VM 時的 VM ID
+    vmid = create_vm(user_id) # 建立 VM 時的 VM ID
     data = request.get_json()
 
-    new_vmid  = vmid
-    vm_name   = f"vm-{create_vm(user_id)["vm_name"]}"  # 取得 VM 名稱
+    new_vmid  = vmid["vmid"]  # 取得新 VM 的 ID
+    vm_name   = f"vm-{vmid ["vm_name"]}"  # 取得 VM 名稱
     password  = data.get("password","1234")
 
     # 1. 建 VM
